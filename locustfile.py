83b19665f66c96b58ea1a1dde6144b4e933e41cb
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-08-13 16:17:45 -0700

    Implement load testing

---
+++ locustfile.py
@@ -0,0 +1,39 @@
+import yaml
+import urllib3
+from locust import HttpLocust, TaskSet, task
+
+
+# Load config file
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+with open('config.yaml', 'r') as config_file:
+    try:
+        config = yaml.load(config_file)
+        api_base_url = config['api_base_url']
+        term_id = config['term_id']
+        osu_id = config['osu_id']
+        auth = (config['user'], config['password'])
+    except yaml.YAMLError as error:
+        exit(error)
+
+
+class UserBehavior(TaskSet):
+    @task(1)
+    def get_by_osuId_id(self):
+        url = '{}?osuId={}'.format(api_base_url, osu_id)
+        self.client.get(url, verify=False, auth=auth)
+
+    @task(3)
+    def get_by_term_id(self):
+        url = '{}?term={}'.format(api_base_url, term_id)
+        self.client.get(url, verify=False, auth=auth)
+
+    @task(1)
+    def get_by_id(self):
+        url = '{}/{}'.format(api_base_url, '{}-{}'.format(osu_id, term_id))
+        self.client.get(url, verify=False, auth=auth)
+
+
+class ApiUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 100
+    max_wait = 5000
