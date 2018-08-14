1e8621c868e9f34ef0ec57d52cb1c3638b5c0bdd
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-08-14 16:06:47 -0700

    Implement all test cases

--- locustfile.py
+++ locustfile.py
@@ -17,7 +17,7 @@ with open('config.yaml', 'r') as config_file:
 
 
 class UserBehavior(TaskSet):
-    @task(1)
+    @task(2)
     def get_by_osuId_id(self):
         url = '{}?osuId={}'.format(api_base_url, osu_id)
         self.client.get(url, verify=False, auth=auth)
@@ -28,9 +28,29 @@ class UserBehavior(TaskSet):
         self.client.get(url, verify=False, auth=auth)
 
     @task(1)
+    def get_by_bad_request(self):
+        url = '{}'.format(api_base_url)
+        with self.client.get(
+            url,
+            catch_response=True, verify=False, auth=auth
+        ) as response:
+            if response.status_code == 400:
+                response.success()
+
+    @task(2)
     def get_by_id(self):
         url = '{}/{}'.format(api_base_url, '{}-{}'.format(osu_id, term_id))
         self.client.get(url, verify=False, auth=auth)
+
+    @task(1)
+    def get_by_bad_id(self):
+        url = '{}/{}'.format(api_base_url, 'invalid_id')
+        with self.client.get(
+            url,
+            catch_response=True, verify=False, auth=auth
+        ) as response:
+            if response.status_code == 404:
+                response.success()
 
 
 class ApiUser(HttpLocust):
