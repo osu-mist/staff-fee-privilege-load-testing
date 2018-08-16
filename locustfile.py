c3690c0c447e586582c4ea28f7bb9138846ed727
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-08-16 10:46:52 -0700

    Adjust load test

--- locustfile.py
+++ locustfile.py
@@ -1,3 +1,4 @@
+import random
 import yaml
 import urllib3
 from locust import HttpLocust, TaskSet, task
@@ -9,23 +10,38 @@ with open('config.yaml', 'r') as config_file:
     try:
         config = yaml.load(config_file)
         api_base_url = config['api_base_url']
-        term_id = config['term_id']
-        osu_id = config['osu_id']
         auth = (config['user'], config['password'])
     except yaml.YAMLError as error:
         exit(error)
 
+term_ids = [f'20{y:02}0{t}' for y in range(15, 20) for t in range(0, 4)]
+osu_ids = set()
+
 
 class UserBehavior(TaskSet):
+    # get some ids before run the task set
+    def on_start(self):
+        self.get_by_term_id()
+
     @task(2)
     def get_by_osuId_id(self):
+        osu_id = random.sample(osu_ids, 1)[0]
         url = f'{api_base_url}?osuId={osu_id}'
         self.client.get(url, verify=False, auth=auth)
 
-    @task(3)
+    @task(2)
     def get_by_term_id(self):
+        term_id = random.choice(term_ids)
         url = f'{api_base_url}?term={term_id}'
-        self.client.get(url, verify=False, auth=auth)
+        with self.client.get(
+            url,
+            catch_response=True, verify=False, auth=auth
+        ) as response:
+            if response.status_code == 200:
+                if response.json()['data'] and len(osu_ids) < 10:
+                    data = random.choice(response.json()['data'])
+                    osu_ids.add(data['id'][:9])
+                response.success()
 
     @task(1)
     def get_by_bad_request(self):
@@ -37,19 +53,16 @@ class UserBehavior(TaskSet):
             if response.status_code == 400:
                 response.success()
 
-    @task(2)
-    def get_by_id(self):
-        url = f'{api_base_url}/{osu_id}-{term_id}'
-        self.client.get(url, verify=False, auth=auth)
-
     @task(1)
-    def get_by_bad_id(self):
-        url = f'{api_base_url}/invalid_id'
+    def get_by_id(self):
+        term_id = random.choice(term_ids)
+        osu_id = random.sample(osu_ids, 1)[0]
+        url = f'{api_base_url}/{osu_id}-{term_id}'
         with self.client.get(
             url,
             catch_response=True, verify=False, auth=auth
         ) as response:
-            if response.status_code == 404:
+            if response.status_code == 200 or response.status_code == 404:
                 response.success()
 
 
